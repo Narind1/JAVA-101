01157b5dda4991f258201aa1d04c8c958925abd5
Narind1 <ash.softech@gmail.com>
2025-03-21 14:18:39 +0530

    added program

---
+++ JavaProgrmas/B.java
@@ -0,0 +1,14 @@
+package Q;
+import p.A;
+class B{
+    public static void main(String[] args) {
+        A a1 = new A();
+        a1.A1();
+        A a2 = new A();
+        a2.A2();
+        A a3 = new A();
+        a3.A3();
+        A a4 = new A();
+        a4.A4();
+    }
+}
